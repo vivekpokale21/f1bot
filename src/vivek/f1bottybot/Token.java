1d6aaa7e7c3dbe2377ecc6327f9c2568225793cb
VivekPokale21 <38533762+VivekPokale21@users.noreply.github.com>
2022-04-01 20:08:50 +0400

    Add files via upload

--- src/vivek/f1bottybot/Token.java
+++ src/vivek/f1bottybot/Token.java
@@ -1,5 +1,5 @@
 package vivek.f1bottybot;
 
 public class Token {
-    static String token = "OTUxODg5MjAzNTgxNTc5MzA0.YiuB5w.0_VaWmTayPlSFYp2CaK__9nn_PU";
+    static String token = System.getenv("TOKEN");
 }
